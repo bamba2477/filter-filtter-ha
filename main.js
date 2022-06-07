211b900ecbbccc98c4d8bf1a24e5558af5acf871
bamba2477 <94611436+bamba2477@users.noreply.github.com>
2022-06-07 10:39:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var Content;
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    recognition.start();
+} 
+
+
+camera = document.getElementById("camera");
+Webcam.set({
+    width:500,
+    height:400,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+
+
+function speak(){
+setTimeout(){
+    img.id = "png.Ahan1"
+    take_snapshot();
+    speak data = "taking 3 selfies in ten seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+}
+
+    
+    var synth = window.speechSynthesis;
+    Webcam.attach(camera);
+
+    speak_data = "Taking your next Selfie in 5 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+
+}
